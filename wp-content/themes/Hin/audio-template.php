92f6c8e22af79f1559f46bf292ab5918e280865b
Magnus <magryd63@hotmail.com>
2013-01-26 18:36:30 +0100

    Added Json Plugin. Posts can now be retrieved through jquery getJson. Example on audio posts page template. Updated random function for displaying audioposts. audioposts. Needs to be

--- wp-content/themes/Hin/audio-template.php
+++ wp-content/themes/Hin/audio-template.php
@@ -6,73 +6,75 @@ Template Name: audio-template
 <?php get_header(); ?>
 
 <script type="text/javascript">
-jQuery(document).ready(function($){	
-generateGrid(3);
-appendToGrid();
+
+jQuery.noConflict();
+jQuery(document).ready(function(jQuery){	
+//generateGrid(3);
+//appendToGrid();
+//var root = location.protocol + '//' + location.host;
+ jQuery.getJSON('http://hinmusic.com.localhost/?json=1',
+  {
+    'status': 'ok',
+	'post_type':'audiopost',
+	'count': '20'
+  },
+  function(data) {
+   var posts = data;
+   createcontainers(data);
+   appendpostfromjson(data);
+  });
 });
 
-function generateGrid(columns){
-var test =  jQuery('.entry-content-circle').length;
-var rows = parseInt(test/2 + 1);
-var grid = jQuery('<table>').addClass('randomGrid');
- for(i=0;i<rows;i++){
-   var row = jQuery('<tr>').addClass('randomGrid-row'); 
-   for(j=0;j<columns;j++){
-	var column = jQuery('<td>').addClass('randomGrid-column');
-	row.append(column);
-   }
-   grid.append(row);
-   }
-   jQuery('#content').append(grid);
-   appendToGrid();
+function createcontainers(data){
+ jQuery.each(data.posts, function(i,post){
+      var randomGridrow = jQuery('<div class="randomGrid-row">');
+	  var randomGridcolumnleft = jQuery('<div class="randomGrid-column">');
+	  var randomGridcolumnmiddle = jQuery('<div class="randomGrid-column">');
+	  var randomGridcolumnright = jQuery('<div class="randomGrid-column">');
+	  randomGridrow.append(randomGridcolumnleft);
+	  randomGridrow.append(randomGridcolumnmiddle);
+	  randomGridrow.append(randomGridcolumnright);
+	  randomGridrow.appendTo('#content');	  
+    });
 }
-function appendToGrid(){
-var content =  jQuery('.entry-content-circle').clone();
-jQuery('.randomGrid tr').each( function(){
-	jQuery(this).children('td').each(function() {
-	
-	//jQuery(this).append(post);	
-	});
-});
 
+function appendpostfromjson(data){
+	var posts = [];
+	jQuery.each(data.posts, function(i,post){
+		var circlecontainer = jQuery('<div align="center" class="entry-content-circle">');
+		var circletitle = jQuery('<div class="main-entry-title-circle">'); 
+        var circlecontent = jQuery('<div>');		
+		circletitle.html(post.title);
+		circlecontent.html(post.content);
+		circlecontainer.append(circletitle);
+		circlecontainer.append(circlecontent);
+		var entrymeta = jQuery('<div class="entry-meta" style="float:right">');
+		entrymeta.html(post.date);
+		circlecontainer.append(entrymeta);
+		posts.push(circlecontainer);				
+		});	
+	
+	jQuery('.randomGrid-row').each(function(){
+		jQuery(this).find('.randomGrid-column').each(function(){		
+			if(posts.length > 0){
+				var randomInt = getRandomInt(1,10);				
+				if(randomInt > 3){				
+					var appendpost = posts.shift();		
+					jQuery(this).append(appendpost);
+				}
+			}
+		});
+	});
+	
+}
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 </script>		
         <div id="primary-fixed-large" class="content-area">
             <div id="content" class="site-content" role="main">				
-				<?php $args = array( 'post_type' => 'audiopost', 'posts_per_page' => 20 );
-				$loop = new WP_Query( $args );
-				$c = 0;
-				$clastpost = 0;
-				while ( $loop->have_posts() ) : $loop->the_post(); $c++; $clastpost = $c; ?>
-				<?php if( $c == 1) {?>				
-				 <div class="rowfull">				 
-				<?php } else if($c == 4){?>				
-				<div class="rowpair">					
-				<?php } ?>				
-					<div align="center" class="entry-content-circle float-left">
-						<div class="main-entry-title-circle">				
-						 <?php the_title();?>		
-					    </div>
-				 <?php the_excerpt();?>				
-				 <a class="comment-link" href="<?php comments_link(); ?>">
-				   Mer..
-				 </a>
-				 <?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-				 <div class="entry-meta" style="float:right">
-					<?php shape_posted_on(); ?>
-				 </div><!-- .entry-meta -->				
-				</div>				
-				<?php if( $c == 3) {				
-					    echo '</div>'; }
-					  else if($c == 5){
-			            echo '</div>'; 
-			            $c = 0; }
-				endwhile;?>			
-				<?php
-				if($clastpost != 3 || $clastpost != 5) {				
-					    echo '</div>'; }					  
-				?>				
+				
             </div><!-- #content .site-content -->
         </div><!-- #primary .content-area --> 
 
