de143e5f52a51bec4e2f04f53555ef5243b79a1f
Fredrik Söderström <tirithen@gmail.com>
2013-02-21 00:57:18 +0100

    Added members and mambers listing page

---
+++ wp-content/themes/hin/memberslist-template.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Members listing template
+*/
+?>
+
+<?php get_header(); ?>
+<div id="primary" class="content-area">
+	<h1><?php the_title();?></h1>
+	<ul id="content" role="main">
+		<?php
+			$args = array('post_type' => 'member', 'posts_per_page' => -1);
+			$loop = new WP_Query($args);
+			if (have_posts()) : while ($loop->have_posts()) : $loop->the_post();
+		?>
+			<li>
+				<div class="entry-content-circle">
+					<h2><?php the_title();?></h2>
+					<?php the_content();?>
+				</div>
+				<?php
+				if (has_post_thumbnail()) {
+					//~ the_post_thumbnail(array('class' => 'entry-content-circle'));
+					the_post_thumbnail(array(300, 300));
+				}
+				?>
+			</li>
+		<?php endwhile; endif;?>
+	</ul>
+</div>
+<?php get_footer(); ?>
+
