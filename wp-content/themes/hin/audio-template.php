618b63899090387f5d60475f70c19c1cb1211e06
Magnus <magryd63@hotmail.com>
2013-02-23 13:00:05 +0100

    small update to  audiotemplate css

--- wp-content/themes/hin/audio-template.php
+++ wp-content/themes/hin/audio-template.php
@@ -23,7 +23,7 @@ Template Name: audio-template
 					<div align="center" class="entry-content-circle-medium float-left fadeIn">
 							<h2 class="main-entry-title-circle"><?php the_title();?></h2>
 					<div class="audiocontainer">
-						<?php the_content();?>	
+						<?php the_content();?>						
 					</div>
 					<a class="comment-link" href="<?php comments_link(); ?>">
 						Mer..
