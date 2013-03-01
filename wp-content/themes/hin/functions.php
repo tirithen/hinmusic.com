d0df2a0bdcd1884ca0ff4a00e36be0abb3182ea4
Fredrik Söderström <tirithen@gmail.com>
2013-03-01 20:05:03 +0100

    Disabled scripts that is not used on the site for now

--- wp-content/themes/hin/functions.php
+++ wp-content/themes/hin/functions.php
@@ -113,27 +113,26 @@ add_action( 'widgets_init', 'hin_widgets_init' );
  */
 function hin_scripts() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_style( 'shadowbox', get_template_directory_uri() . '/js/shadowbox-3.0.3/shadowbox.css');
-	wp_enqueue_style( 'codaslider', get_template_directory_uri() . '/js/codaslider/css/coda-slider.css');
-	wp_enqueue_style( 'basicslider', get_template_directory_uri() . '/js/basicslider/bjqs.css');
+	//~ wp_enqueue_style( 'shadowbox', get_template_directory_uri() . '/js/shadowbox-3.0.3/shadowbox.css');
+	//~ wp_enqueue_style( 'codaslider', get_template_directory_uri() . '/js/codaslider/css/coda-slider.css');
+	//~ wp_enqueue_style( 'basicslider', get_template_directory_uri() . '/js/basicslider/bjqs.css');
 
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );    }
+    //~ if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        //~ wp_enqueue_script( 'comment-reply' );    }
 
-    wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
-	wp_enqueue_script( 'shadowbox', get_template_directory_uri() . '/js/shadowbox-3.0.3/shadowbox.js', array( 'jquery' ), '20120206', true );
-    wp_enqueue_script( 'codaslider', get_template_directory_uri() . '/js/codaslider/js/jquery.coda-slider-3.0.js', array( 'jquery' ), '20120206', true );
-	wp_enqueue_script( 'basicslider', get_template_directory_uri() . '/js/basicslider/js/bjqs-1.3.js', array( 'jquery' ), '20120206', true );
-	wp_enqueue_script( 'flippy', get_template_directory_uri() . '/js/flippy/jquery.flippy.js', array( 'jquery' ), '20130101', true );
-	wp_enqueue_script( 'basicsliderInit', get_template_directory_uri() . '/js/jquery.basicsliderInit.js', array( 'jquery' ), '20130219', true );
-	wp_enqueue_script( 'shadowboxInit', get_template_directory_uri() . '/js/jquery.shadowboxInit.js', array( 'jquery' ), '20130219', true );
+    //~ wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
+	//~ wp_enqueue_script( 'shadowbox', get_template_directory_uri() . '/js/shadowbox-3.0.3/shadowbox.js', array( 'jquery' ), '20120206', true );
+    //~ wp_enqueue_script( 'codaslider', get_template_directory_uri() . '/js/codaslider/js/jquery.coda-slider-3.0.js', array( 'jquery' ), '20120206', true );
+	//~ wp_enqueue_script( 'basicslider', get_template_directory_uri() . '/js/basicslider/js/bjqs-1.3.js', array( 'jquery' ), '20120206', true );
+	//~ wp_enqueue_script( 'flippy', get_template_directory_uri() . '/js/flippy/jquery.flippy.js', array( 'jquery' ), '20130101', true );
+	//~ wp_enqueue_script( 'basicsliderInit', get_template_directory_uri() . '/js/jquery.basicsliderInit.js', array( 'jquery' ), '20130219', true );
+	//~ wp_enqueue_script( 'shadowboxInit', get_template_directory_uri() . '/js/jquery.shadowboxInit.js', array( 'jquery' ), '20130219', true );
 
-	if ( is_page_template('audio-template.php') ) {
-	//Add jquery eventsubscription on audio template.
-	wp_enqueue_script( 'sCloudwidget','http://w.soundcloud.com/player/api.js', array( 'jquery' ), '20130219', true );
-	wp_enqueue_script( 'setAudioEvents', get_template_directory_uri() . '/js/jquery.setAudioEvents.js', array( 'jquery' ), '20130219', true );
-
-	}
+	//~ if ( is_page_template('audio-template.php') ) {
+	//~ //Add jquery eventsubscription on audio template.
+        //~ wp_enqueue_script( 'sCloudwidget','http://w.soundcloud.com/player/api.js', array( 'jquery' ), '20130219', true );
+        //~ wp_enqueue_script( 'setAudioEvents', get_template_directory_uri() . '/js/jquery.setAudioEvents.js', array( 'jquery' ), '20130219', true );
+	//~ }
 }
 add_action( 'wp_enqueue_scripts', 'hin_scripts' );
 
